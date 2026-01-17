4173cb76f309f5fb15ecadb11ef94a2a92b4f9b6
pegalvao <galvaoevaristo@gmail.com>
2026-01-16 21:13:07 -0300

    feature: Login tests complete and Logout tests init

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,34 @@
-describe('Login test', () => {
-  it('Login válido', () => {
+// describe('Login test', () => {
+//   it('Login válido', () => {
+//     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+//     cy.get('[name="username"]').type('Admin'),
+//     cy.get('[name="password"]').type('admin123'),
+//     cy.get('.oxd-button').click(),
+//     cy.get('.oxd-input').should('be.visible')
+//   })
+
+//    it('Login inválido', () => {
+//     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+//     cy.get('[name="username"]').type('Teste123'),
+//     cy.get('[name="password"]').type('Teste123'),
+//     cy.get('.oxd-button').click()
+//   })
+
+// })
+
+describe('Logout test', () => {
+  it('Logout com sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get('[name="username"]').type('Admin'),
-    cy.get('[name="password"]').type('admin123')
+    cy.get('[name="password"]').type('admin123'),
+    cy.get('.oxd-button').click(),
+    cy.get('.oxd-userdropdown-tab > .oxd-icon').click(),
+    cy.get('.oxd-userdropdown').should('be.visible'),
+    cy.get(':nth-child(4) > .oxd-userdropdown-link').click()
+
+
+    // cy.get('.orangeherm-login-slot-wrapper').should('be.visible'),
+    // cy.
   })
+
 })
