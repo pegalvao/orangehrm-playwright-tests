de7af63d08460ab59bf49f69207e0dbc50ea71cf
pegalvao <galvaoevaristo@gmail.com>
2026-01-18 17:17:28 -0300

    feature: Logout test done

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,20 +1,20 @@
-// describe('Login test', () => {
-//   it('Login válido', () => {
-//     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-//     cy.get('[name="username"]').type('Admin'),
-//     cy.get('[name="password"]').type('admin123'),
-//     cy.get('.oxd-button').click(),
-//     cy.get('.oxd-input').should('be.visible')
-//   })
+describe('Login test', () => {
+  it('Login válido', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Admin'),
+    cy.get('[name="password"]').type('admin123'),
+    cy.get('.oxd-button').click(),
+    cy.get('.oxd-input').should('be.visible')
+  })
 
-//    it('Login inválido', () => {
-//     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-//     cy.get('[name="username"]').type('Teste123'),
-//     cy.get('[name="password"]').type('Teste123'),
-//     cy.get('.oxd-button').click()
-//   })
+   it('Login inválido', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Teste123'),
+    cy.get('[name="password"]').type('Teste123'),
+    cy.get('.oxd-button').click()
+  })
 
-// })
+})
 
 describe('Logout test', () => {
   it('Logout com sucesso', () => {
@@ -24,11 +24,10 @@ describe('Logout test', () => {
     cy.get('.oxd-button').click(),
     cy.get('.oxd-userdropdown-tab > .oxd-icon').click(),
     cy.get('.oxd-userdropdown').should('be.visible'),
-    cy.get(':nth-child(4) > .oxd-userdropdown-link').click()
-
-
-    // cy.get('.orangeherm-login-slot-wrapper').should('be.visible'),
-    // cy.
+    cy.wait(1000),
+    cy.get(':nth-child(4) > .oxd-userdropdown-link').click(),
+    cy.get('[name="username"]').should('be.visible')
+    
   })
 
 })
