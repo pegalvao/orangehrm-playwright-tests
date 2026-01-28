994da1d3d556b58e0e6140d2b2448e2555d85387
pegalvao <galvaoevaristo@gmail.com>
2026-01-27 22:36:42 -0300

    feature: Admin initial tests

--- cypress/e2e/Admin/createUser.cy.js
+++ cypress/e2e/Admin/createUser.cy.js
@@ -1,6 +1,20 @@
-describe('Interface test', () => {
-  it('Verificar elementos da interface', () => {  
-  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+describe('Admin test', () => {
+    beforeEach(() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('.oxd-button').click()
+        cy.url().should('include', '/dashboard')
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers')
+    })
+        
+//   it('Verificar elementos da interface', () => {  
+//      cy.url().should('include', '/admin/viewSystemUsers')
+//     })
 
-  })
+  it('Criar novo usuário', () => {  
+     cy.url().should('include', '/admin/viewSystemUsers')
+    })
+
+
 })
