acab84607776e6815fe4061a3bc43e99d93aac3a
pegalvao <galvaoevaristo@gmail.com>
2026-07-07 22:31:04 -0300

    feat: createUser Initial teste : new User admin with Employee Name

--- cypress/e2e/Admin/UserManagement/createUser.cy.js
+++ cypress/e2e/Admin/UserManagement/createUser.cy.js
@@ -13,9 +13,33 @@ describe('Admin test', () => {
     it('Criar novo usuário com função de admin caso já tenha um Employee Name que já exista', () => {
         cy.get('.orangehrm-header-container .oxd-button').contains('Add').click()
         cy.fixture('userManagement').then((userManagement) => {
-            cy.get('.oxd-input-group .oxd-input').eq(0).type(userManagement.employee1.username)
-            cy.get('.oxd-input-group .oxd-input').eq(1).type(userManagement.employee1.password)
-            cy.get('.oxd-input-group .oxd-input').eq(2).type(userManagement.employee1.password)
+
+            //User Role
+            cy.contains('.oxd-label','User Role').parents('.oxd-input-group').find('.oxd-select-text').click()
+            cy.contains('.oxd-select-option', 'Admin').click()
+
+            //EmployeeName
+            cy.contains('.oxd-label', 'Employee Name').parents('.oxd-input-group').find('input').type('Test First Demo')
+
+            cy.get('.oxd-autocomplete-dropdown').should('be.visible').contains('Test First Demo').click()
+            
+            // Status
+            cy.contains('.oxd-label', 'Status').parents('.oxd-input-group').find('.oxd-select-text').click()
+            cy.contains('.oxd-select-option', 'Enabled').click()
+
+            // Username
+            cy.contains('.oxd-label', 'Username').parents('.oxd-input-group').find('input').type(userManagement.employee1.username)
+
+            // Password 
+            cy.contains('.oxd-label', 'Password').parents('.oxd-input-group').find('input').type(userManagement.employee1.password)
+
+            //Confirm Password
+            
+            cy.contains('.oxd-label', 'Confirm Password') .parents('.oxd-input-group').find('input').type(userManagement.employee1.password)
+
+
+            cy.contains('button', 'Save').click()
+            cy.get('.oxd-toast').should('be.visible').and('contain.text', 'Successfully Saved')
         })
     })
 
